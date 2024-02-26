cbf25718be51052b668c3a6217ffe34722013276
jiaero <malns7@naver.com>
2024-02-26 10:22:47 +0900

    function prac

--- 04_JS/function_prac.js
+++ 04_JS/function_prac.js
@@ -1,9 +1,18 @@
 function multifly(num1, num2) {
   return num1 * num2;
+  // return 문 다음 코드는 실행되지 않는다.
 }
 
 console.log(multifly(3, 7));
 console.log(multifly(2, 2));
+
+// square 함수 만들기
+function square(num3) {
+  return num3 ** 2;
+}
+
+console.log(square(4));
+
 
 const square = function (num) {
   console.log(num ** 2);
