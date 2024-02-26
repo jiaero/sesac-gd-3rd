16895dda7f775016fee02eda38ac080cc987ae41
jiaero <malns7@naver.com>
2024-02-26 10:17:09 +0900

    js function prac

---
+++ 04_JS/function_prac.js
@@ -0,0 +1,12 @@
+function multifly(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multifly(3, 7));
+console.log(multifly(2, 2));
+
+const square = function (num) {
+  console.log(num ** 2);
+};
+
+square(4);
