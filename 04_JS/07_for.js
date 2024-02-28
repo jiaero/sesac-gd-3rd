d5ab44d58217fef54b5946da7fc431d0913681e6
jiaero <malns7@naver.com>
2024-02-28 14:14:14 +0900

    JS for문

--- 04_JS/07_for.js
+++ 04_JS/07_for.js
@@ -112,23 +112,131 @@ for (let i = 1; i < 10; i++) {
 }
 
 // 이중 for문
-fot (let i = 0; i < 5; i++) {
-    console.log('i :', i);
+for (let i = 0; i < 5; i++) {
+  console.log("i :", i);
 
-    for (let j; j < 10; j++) {
-        console.log(`현재 i는 ${i}이고, j는 ${j}입니다.`)
-    }
+  for (let j; j < 10; j++) {
+    console.log(`현재 i는 ${i}이고, j는 ${j}입니다.`);
+  }
+}
+
+// // 0 ~ 100의 숫자 중에서 2 또는 5의 배수 총합 구하기
+
+// let sum = 0;
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0 || i % 5 === 0 ) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+console.log("-----------------------");
+
+// while 문
+/*
+  while (조건) {
+    조건에 만족하는 동안 실행될 코드
+  }
+*/
+// 조건을 제어하는 구문이 없기 때문에 무한 루프에 빠지지 않도록 주의해서 사용
+let num = 1;
+while (num <= 9) {
+  console.log(num);
+  num++; // num = num + 1
+}
+
+// while을 사용해 9부터 4까지 콘솔에 찍기
+num = 9;
+while (num >= 4) {
+  console.log(num);
+  num--; // num = num - 1
+}
+
+console.log("-----------------------");
+
+// while문 사용해 1부터 10까지 짝수 출력
+num = 1;
+while (num <= 10) {
+  if (num % 2 === 0) {
+    console.log(num);
+  }
+  num++;
+}
+
+// 10부터 1까지 홀수 출력
+
+num = 10;
+while (num >= 1) {
+  if (num % 2 === 1) {
+    console.log(num);
+  }
+  num--;
+}
+
+console.log("-----------------------");
+
+// 무한루프 -> 조건식이 참이면 무한히 while문을 빠져나올 수 없음
+let num2 = 0;
+while (true) {
+  console.log(num2);
+  num2++;
+
+  // break; // break 없다면 무한루프 또는 빠져나가야 할 조건을 설정
+  // num2가 11이 되는 순간 아래의 조건이 참이 되면서 while문 종료
+  if (num2 > 10) {
+    break;
+  }
+}
+
+num2 = 0;
+while (confirm("계속 할까요?")) {
+  // confirm의 확인 버튼은 true, 취소 버튼은 false를 반환
+  // 취소 버튼 클릭 시 조건이 false가 되어 반복문 종료
+  num2++;
+  alert(`${num2}번째 alert 창`);
 }
 
 // 0 ~ 100의 숫자 중에서 2 또는 5의 배수 총합 구하기
 
+// for문
+
 let sum = 0;
 
 for (let i = 0; i <= 100; i++) {
-  if (i % 2 === 0 || i % 5 === 0 ) {
+  if (i % 2 === 0 || i % 5 === 0) {
     sum += i;
   }
 }
-console.log(sum)
+console.log(sum);
 
+// while문
+
+i = 0;
+sum = 0;
+while (i <= 100) {
+  if (i % 2 == 0 || i % 5 == 0) {
+    sum += i; // sum = sum + i
+  }
+  i++;
+}
+console.log(sum);
+
+// 실습 2. 13의 배수이면서 홀수 찾기
+let input = Number(prompt('숫자를 입력해주세요.'));
+
+for(let i = 0; i <= input; i++) {
+  if (i % 13 === 0 && i % 2 === 1) {
+    console.log(i);
+  }
+}
+
+// 실습 3. 이중 for문을 사용해 구구단 출력
+// i는 구구단의 단, j는 단에 곱해줄 수
+for (let i = 2; i <= 9; i++) {
+  console.log(`---${i} 단---`);
+  for (let j = 1; j <= 9; j++) {
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
 
