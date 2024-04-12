ceed2af6456db7c975e8170c7ce384f55431be72
jiaero <malns7@naver.com>
2024-04-12 16:42:31 +0900

    react ref

---
+++ 13_react_ref/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import FuncRef from "./components/FuncRef";
+import ClassRef from "./components/ClassRef";
+import Prac1 from "./components/Prac1";
+
+function App() {
+  return (
+    <div className="App">
+      <FuncRef />
+      <ClassRef />
+      <Prac1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ 13_react_ref/src/components/ClassRef.jsx
@@ -0,0 +1,29 @@
+import { Component, createRef } from "react";
+
+export default class ClassRef extends Component {
+  // 🔽 input = document.querySelector('input') 와 동일
+  input = createRef();
+  localVar = createRef();
+  state = {
+    state: 0,
+  };
+
+  render() {
+    return (
+      <>
+        <h2>클래스형 컴포넌트에서 ref 사용</h2>
+        <input type="text" ref={this.input} />
+        <button onClick={() => this.input.current.focus()}>버튼</button>
+
+        <br />
+
+        <div>this.localVar.current : {this.localVar.current}</div>
+        <div>this.state.state : {this.state.state}</div>
+        <button onClick={() => this.localVar.current++}>localVar + 1</button>
+        <button onClick={() => this.setState({ state: this.state.state + 1 })}>
+          state + 1
+        </button>
+      </>
+    );
+  }
+}
