955340a2a06dc9d25fd6357d6948319da748ae56
jiaero <malns7@naver.com>
2024-04-15 15:50:32 +0900

    react useCallback

---
+++ 15_react_hooks/src/component/UseCallback2.jsx
@@ -0,0 +1,44 @@
+import { useCallback } from "react";
+import { useState } from "react";
+
+export default function UseCallback2() {
+  const [text, setText] = useState("");
+
+  // input 태그에 onChange 속성으로 연결된 함수의 경우
+  // 업데이트가 매우 잦아 리렌더링 될 때마다 이벤트 핸들러 함수가 생성됨
+  // 로직 자체에는 아무 변화가 없지만 불필요하게 계속 함수가 생성되는 중
+  //   const handleChange = (e) => {
+  //     setText(e.target.value);
+  //   };
+
+  // useCallback hook을 사용해서 콜백 함수 자체를 저장하고, 사용
+  // 불필요하게 함수가 생성되지 않음
+  // 의존성 배열이 빈 배열인 경우 useCallback hook을 사용한 함수는
+  // 다시 선언되지 않음
+  const handleChange = useCallback((e) => {
+    setText(e.target.value);
+  }, []);
+
+  return (
+    <>
+      <h2>useCallback 사용 2</h2>
+      <input type="text" value={text} onChange={handleChange} />
+      <div>text state : {text || '없음'}</div>
+    </>
+  );
+}
+
+// useMemo vs useCallback
+
+// const memoizationValue = useMemo(() => function, [depency arr]);
+// const memoizationCallback = useCallback(() => function, [depency arr]);
+
+// useCallback은 useMEMO를 기반으로 만들어진 Hook
+// useCallback은 useMEMO에서 "값"이 아닌 "함수를 사용할 때"의
+// 편의성을 높이기 위해 만들어진 Hook
+
+// 공통점 : 성능 최적화
+// 차이점 : 특정 "값"(useMemo)를 재사용할지, 특정 "함수"(useCallback)를 재사용할지에 따라 선택적으로 사용
+
+// useMemo는 값의 재사용을 위해 전달된 함수를 실행하고, "그 결과(리턴값)"를 메모이제이션
+// useCallback은 함수의 재사용을 위해 전달된 "함수 자체"를 메모이제이션
